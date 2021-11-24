6eb53956107ae173ec5a51d33c11916fe995a787
lecbit <artm-marych@rambler.ru>
2021-11-24 17:32:06 +0200

    ~ex2-71~ Комп привязка к свойствам

---
+++ ekzamen-2/simplecomp.exam.71/index.php
@@ -0,0 +1,18 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Простой компонент-ex2-71");
+?><?$APPLICATION->IncludeComponent(
+	"exam2:simplecomp.exam.71", 
+	".default", 
+	array(
+		"COMPONENT_TEMPLATE" => ".default",
+		"PRODUCTS_IBLOCK_ID" => "2",
+		"CLASSIF_IBLOCK_ID" => "7",
+		"TEMPLATE" => "",
+		"PROPERTY_CODE" => "FIRMA",
+		"CACHE_TYPE" => "A",
+		"CACHE_TIME" => "36000000",
+		"CACHE_GROUPS" => "Y"
+	),
+	false
+);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
