9c4d8b39b96b73c4f9b7ff0f7080a1e59b8bff28
lecbit <artm-marych@rambler.ru>
2021-12-13 12:54:13 +0200

    ~ex2-88~ Проверка кеша

---
+++ ekzamen-2/otsenka-proizvoditelnosti-88/index.php
@@ -0,0 +1,6 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Оценка производительности 88");
+?>
+Самая ресурсоемкая страница : /products/index.php . Доля загрузки: 20.72%
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
